7426e08fae5df8a1eecc7b3af9e46fd1c28cbf81
ahmad-elkhawaldeh <60712751+ahmad-elkhawaldeh@users.noreply.github.com>
2022-01-04 19:16:29 -0500

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 /**.
  * Pattern
  */
-public class Pattern {
+final class Pattern {
      /**
      * Constant.
      */
@@ -41,7 +41,7 @@ public class Pattern {
     * Function finds the index of a number, using Binary Search recursively.
     * @param number
     * @return printPattern
-    */ 
+    */
     public static final String printPattern(final int number) {
          if (number < ONE) {
              return "";
@@ -68,7 +68,8 @@ public class Pattern {
             System.out.println("Error: Worng input");
         } else {
             System.out.println("User input | Program output");
-            System.out.println(number + "  | " + printPattern(number));            
+            System.out.println(number + "  | " 
+                               + printPattern(number));
             System.out.println("\n");
         }
     }
